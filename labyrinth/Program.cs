4261932c2a25d4a78898f0472425d7b416b2b254
Darya-Podryadova <64641338+Darya-Podryadova@users.noreply.github.com>
2020-09-12 18:37:24 +0500

    Add files via upload

---
+++ labyrinth/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace labyrinth
+{
+    class getInfo
+    {
+      
+    }
+    class 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
