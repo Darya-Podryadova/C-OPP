e40317a7ed473bbd949f2f27213a22bf5f8839a5
Darya-Podryadova <64641338+Darya-Podryadova@users.noreply.github.com>
2020-09-12 18:44:42 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace lab0
         static void Main(string[] args) 
         {
             Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
